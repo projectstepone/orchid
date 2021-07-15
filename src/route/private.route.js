3f6aa9162567c5b2405697e849a2ab795c73bf24
chandrasekhar.patra <chandrasekhar.patra@phonepe.com>
2021-07-15 10:19:15 +0530

    login path fix

--- src/route/private.route.js
+++ src/route/private.route.js
@@ -10,11 +10,13 @@ import {
 
 import { useSelector } from 'react-redux'
 
+const LOGIN_PATH = process.env.REACT_APP_ORCHID_HOST + "/login"
+
 function PrivateRoute({ children, ...rest }) {
   const history = useHistory()
   const isLoggedIn = useSelector(state => state.auth.isLoggedIn)
   if (!isLoggedIn) {
-    window.location.href = "https://592a5ede94d2.ngrok.io/login"
+    window.location.href = LOGIN_PATH
     return null
   }
   return (
